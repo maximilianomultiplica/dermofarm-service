40a4190e11d19b118959447500f5a840565c78ef
Victor Trisac <vtrisac@proaltus.com>
2025-04-03 22:56:09 +0200

    Restaurar archivos de entidades en src/entities para mantener compatibilidad

---
+++ src/entities/order.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+import { Customer } from "./customer.entity";
+import { OrderItem } from "./order-item.entity";
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  dermofarmId: number;
+
+  @ManyToOne(() => Customer, (customer) => customer.orders)
+  customer: Customer;
+
+  @OneToMany(() => OrderItem, (orderItem) => orderItem.order)
+  items: OrderItem[];
+
+  @Column("float")
+  total: number;
+
+  @Column({
+    default: "pending",
+  })
+  status: string;
+
+  @Column({ nullable: true })
+  lastSync: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
